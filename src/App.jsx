bb2acb11ad925ee357b695f39d06ddd12dc367bc
Hehjun Lim <114204941+hehjunlim@users.noreply.github.com>
2024-12-24 16:46:38 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Calendar from './components/Calendar'
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Itinerary for 7 Days in New York</h1>
+      <h2>Welcome! Check out this calendar to get to know the city and see all the sights during your stay.</h2>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
